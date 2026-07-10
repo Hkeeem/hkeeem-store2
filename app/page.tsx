5522c78bf6deb85cfe6ca1d596dd0c51f4927152
hkeeem <Hkeeem@users.noreply.github.com>
2026-07-10 09:23:38 +0300

    Create Home component for main page

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{padding:40, textAlign:'center'}}>
+      <h1 style={{fontSize:32, fontWeight:'bold'}}>متجر حكيم جاهز 🚀</h1>
+      <p style={{marginTop:16}}>تم التصليح - النشر شغال الآن</p>
+    </main>
+  )
+}
