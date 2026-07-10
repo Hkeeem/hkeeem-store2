ef62f1706ceaa18c1d7ecb0d1a085703eb86eb28
hkeeem <Hkeeem@users.noreply.github.com>
2026-07-10 09:22:36 +0300

    Delete app/app directory

--- app/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{padding:40, textAlign:'center'}}>
-      <h1 style={{fontSize:32, fontWeight:'bold'}}>متجر حكيم جاهز 🚀</h1>
-      <p style={{marginTop:16}}>تم إصلاح ملف tailwind والنشر شغال الآن</p>
-    </main>
-  )
-}
