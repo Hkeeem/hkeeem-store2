63260de23ecfcbb85356dff23d99efe157fcb624
hkeeem <Hkeeem@users.noreply.github.com>
2026-07-12 03:43:35 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { useEffect, useState } from "react"
-
+import { supabase } from '@/lib/supabase/client'
 const CATS = [
   { n: "الكل", i: "✨" },
   { n: "سوبرماركت", i: "🛒" },
