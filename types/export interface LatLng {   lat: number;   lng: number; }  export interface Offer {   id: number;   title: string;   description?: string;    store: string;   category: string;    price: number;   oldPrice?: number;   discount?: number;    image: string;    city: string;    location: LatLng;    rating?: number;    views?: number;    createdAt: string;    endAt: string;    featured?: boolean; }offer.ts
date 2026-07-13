4613d2ebf84a1693232d3f3500375cb3e985dc62
hkeeem <Hkeeem@users.noreply.github.com>
2026-07-13 13:56:37 +0300

    Add LatLng and Offer interfaces

---
+++ types/export interface LatLng {   lat: number;   lng: number; }  export interface Offer {   id: number;   title: string;   description?: string;    store: string;   category: string;    price: number;   oldPrice?: number;   discount?: number;    image: string;    city: string;    location: LatLng;    rating?: number;    views?: number;    createdAt: string;    endAt: string;    featured?: boolean; }offer.ts
@@ -0,0 +1,33 @@
+export interface LatLng {
+  lat: number;
+  lng: number;
+}
+
+export interface Offer {
+  id: number;
+  title: string;
+  description?: string;
+
+  store: string;
+  category: string;
+
+  price: number;
+  oldPrice?: number;
+  discount?: number;
+
+  image: string;
+
+  city: string;
+
+  location: LatLng;
+
+  rating?: number;
+
+  views?: number;
+
+  createdAt: string;
+
+  endAt: string;
+
+  featured?: boolean;
+}
