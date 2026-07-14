a5e265e0deab566c0ae4ab2344ba2b2b54174edb
hkeeem <Hkeeem@users.noreply.github.com>
2026-07-14 09:13:08 +0300

    Add Navbar component with branding and greeting

---
+++ components/Navbar.tsx
@@ -0,0 +1,9 @@
+'use client'
+export default function Navbar(){
+ return (
+  <div className="h-14 flex items-center justify-between px-4 bg-white border-b sticky top-0 z-10">
+    <b className="font-black">عروضكم</b>
+    <span className="text-sm">alhkmy11 👋</span>
+  </div>
+ )
+}
