6dd97ec99f3cb89109e0ae34944025ea67e7ade6
hkeeem <Hkeeem@users.noreply.github.com>
2026-07-14 13:54:50 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,9 @@ const nextConfig = {
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'images.unsplash.com' },
-      { protocol: 'https', hostname: '**.supabase.co' },
+      { protocol: 'https', hostname: '*.supabase.co' },
     ],
   },
+  transpilePackages: ['leaflet'],
 }
-
 export default nextConfig
